6bca598fc49f43059949b24517cab5aaf7f7c385
SHIRIN <efshitaria123@gmail.com>
2023-10-18 22:03:10 +0600

    Provider added

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import loginimage from '../../assets/elsa-olofsson-Pm0K9Y3EPUc-unsplash.jpg'
+import regimage from '../../assets/johanne-pold-jacobsen-XYkc3MfT7b4-unsplash.jpg'
 
 const Register = () => {
     return (
         <div className='max-w-6xl mx-auto my-10 h-[80vh]'>
             <div className='text-center'>
                 <h2 className="text-2xl font-medium text-center mb-3">
-                    New Customer 
+                    New Customer
                 </h2>
                 <hr></hr>
             </div>
@@ -14,29 +14,42 @@ const Register = () => {
             <div >
                 <div className="flex flex-col lg:flex-row-reverse justify-between gap-10 py-10">
                     <div className="w-1/2">
-                        <img src={loginimage} alt="loginimage" />
+                        <img src={regimage} alt="regimage" className='w-[550px] h-[380px]' />
                     </div>
                     <div className="card w-1/2 ">
 
-                        <form className="card-body w-2/3 shadow-md bg-base-100 mx-auto">
+                        <form className="card-body w-full shadow-md bg-base-100 mx-auto">
                             <div className='space-y-3'>
-                                <h1 className="text-3xl font-bold">Login now!</h1>
+                                <h1 className="text-3xl font-bold">Sign up!</h1>
                                 <p>
                                     Already have an account? <Link to={'/login'} className='text-[#86198f] underline'>Sign In</Link>
                                 </p>
 
                             </div>
-                            <div className="form-control">
-                                <label className="label">
+                            <div className="form-control flex-row" >
+                                <label className="label w-1/3">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="Full Name" className="input input-bordered w-full" required />
+                            </div>
+                            <div className="form-control flex-row">
+                                <label className="label w-1/3">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="email" className="input input-bordered w-full" required />
                             </div>
-                            <div className="form-control">
-                                <label className="label">
+                            <div className="form-control flex-row">
+                                <label className="label w-1/3">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="url" name="photo" placeholder="photo" className="input input-bordered w-full" required />
+
+                            </div>
+                            <div className="form-control flex-row">
+                                <label className="label w-1/3">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" name="password" placeholder="password" className="input input-bordered w-full" required />
 
                             </div>
                             <div className="form-control mt-6">
---
+++ src/Provider/Provider.jsx
@@ -0,0 +1,72 @@
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../Firebase/Firebase.config";
+import PropTypes from 'prop-types';
+
+
+export const Authcontext = createContext(null)
+const googleprovider = new GoogleAuthProvider();
+const auth = getAuth(app)
+const Provider = ({ children }) => {
+    const [user, setuser] = useState(null)
+    const [loading, setloading] = useState(true)
+
+    const createuser = (email, password) => {
+        setloading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signin = (email, password) => {
+        setloading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const signgoogle = () => {
+        setloading(true)
+        return signInWithPopup(auth, googleprovider)
+    }
+    const logout = () => {
+        setloading(true)
+        return signOut(auth)
+
+    }
+
+    useEffect(() => {
+
+        const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+            if (currentuser) {
+
+                const uid = currentuser.uid;
+
+                setuser(currentuser)
+
+
+                setuser(currentuser)
+                console.log('user', uid)
+                setloading(false)
+            }
+            else {
+                console.log("User is signed out")
+                setloading(false)
+            }
+        });
+        return () => unsubscribe()
+    }, [])
+    const authinfo = {
+        user,
+        createuser,
+        logout,
+        signin,
+        signgoogle,
+        loading
+    }
+    return (
+        <Authcontext.Provider value={authinfo}>
+            {children}
+        </Authcontext.Provider>
+    );
+};
+Provider.propTypes = {
+    children: PropTypes.object,
+};
+
+export default Provider;
